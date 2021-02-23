b188b29d9dea9b2c98cfabf3cef26a6bd1b60743
Orlando Rojas <orlando@able.co>
2021-02-23 18:57:50 -0500

    setup router

---
+++ src/components/common/Container.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Navbar from "./Navbar";
+
+export default function Container({ children }) {
+  return (
+    <Wrapper className="bg-purple h-screen w-screen p-4 ">
+      <Navbar />
+      {children}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  max-width: 610px;
+  max-height: 808px;
+`;
