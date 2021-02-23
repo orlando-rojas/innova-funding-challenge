b2f65c40adce495aa2bbb9df369e5dd324e525c8
Orlando Rojas <orlando@able.co>
2021-02-23 18:53:37 -0500

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#fff",
+      black: "#000",
+      gray: "#DBDBDB",
+      grayLight: "#EDEDED",
+      purple: "#4D2DD9",
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+    },
+    opacity: {
+      0: "0",
+      15: ".15",
+      25: ".25",
+      45: ".45",
+      50: ".5",
+      100: "1",
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
