6eba65c34587b58f93b2d687f66802e7e26e3841
Orlando Rojas <orlando@able.co>
2021-02-24 01:27:14 -0500

    rename cards to boards

--- src/components/card/CardsList.js
+++ src/components/card/CardsList.js
@@ -1,11 +1,11 @@
 import uuid from "react-uuid";
 import Card from "./Card";
 
-export default function CardsList({ cards }) {
+export default function CardsList({ boards }) {
   return (
     <div className="flex mt-3 mb-6 overflow-auto -mx-4">
-      {cards.map((card) => (
-        <Card title={card.title} users={card.users} key={uuid()} />
+      {boards.map((board) => (
+        <Card title={board.title} users={board.users} key={uuid()} />
       ))}
     </div>
   );
